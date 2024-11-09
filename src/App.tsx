c7c1b1678c1179593c27894775c85d71f577d2c7
KashmiraNimnada <kashmiranimnada98@gmail.com>
2024-11-09 11:57:05 +0530

    ProtectedRoute added

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import CreateOrder from './pages/orders/CreateOrder'
 import EditOrder from './pages/orders/EditOrder'
 import { AuthProvider } from './context/AuthContext'
 import Login from './pages/auth/Login'
+import ProtectedRoute from './components/ProtectedRoute'
 
 // parent component
 function App() {
@@ -16,13 +17,15 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="/order/create" element={<CreateOrder />} />
-          <Route path="/order/:id/editOrder" element={<EditOrder />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/order" element={<Order />} />
+            <Route path="/order/create" element={<CreateOrder />} />
+            <Route path="/order/:id/editOrder" element={<EditOrder />} />
+          </Route>
           <Route path="/auth/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
